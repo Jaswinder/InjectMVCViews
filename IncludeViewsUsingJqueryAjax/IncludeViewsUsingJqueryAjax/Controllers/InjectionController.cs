a832a7fdd400cb842f2613223e3e0c2a51949de8
Jaswinder Singh <jpc_rajput@rediffmail.com>
2017-12-28 12:33:42 +0530

    First Commit For Inject MVC Views Inside another View

---
+++ IncludeViewsUsingJqueryAjax/IncludeViewsUsingJqueryAjax/Controllers/InjectionController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IncludeViewsUsingJqueryAjax.Controllers
+{
+    public class InjectionController : Controller
+    {
+        // GET: Injection
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult FirstChild()
+        {
+            return View();
+        }
+        public ActionResult SecondChild()
+        {
+            return View();
+        }
+        public ActionResult ThirdChild()
+        {
+            return View();
+        }
+    }
+}
